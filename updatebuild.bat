269ad44b228b93a995c7086a54152027a386a0ee
Quentin Brooks <yeerkkiller1@gmail.com>
2015-01-25 11:20:06 -0800

    Updatebuild should really only clone the repos, if the user wants to pull them they can do that with bash multi git pull.

--- updatebuild.bat
+++ updatebuild.bat
@@ -1,13 +1,5 @@
 cd ..
-
 cd ..
+
 git clone https://github.com/yeerkkiller1/Interfaces
-cd Interfaces
-chdir
-git pull
-
-cd ..
 git clone https://github.com/yeerkkiller1/Extensions
-cd Extensions
-chdir
-git pull
