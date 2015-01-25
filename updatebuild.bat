a9e72ac8a8e4bfe55894dcf79447f8a47f04bee7
Quentin Brooks <yeerkkiller1@gmail.com>
2015-01-25 10:55:50 -0800

    Flat dependency structures are much better.

---
+++ updatebuild.bat
@@ -0,0 +1,13 @@
+cd ..
+
+cd ..
+git clone https://github.com/yeerkkiller1/Interfaces
+cd Interfaces
+chdir
+git pull
+
+cd ..
+git clone https://github.com/yeerkkiller1/Extensions
+cd Extensions
+chdir
+git pull
